93f8d2bbfa00434e28a2b003f3f69f1495c6098b
Shuhei Hayakawa <shuhei.hayakawa.c8@tohoku.ac.jp>
2024-09-04 22:42:50 +0900

    Modify for 11.2.2

--- include/S2STransportation.hh
+++ include/S2STransportation.hh
@@ -1,28 +1,24 @@
-/*
-  "S2STransportation.hh"
-  
-  Modified by Toshi Gogami , 11Nov2014
-*/
+// -*- C++ -*-
 
 #ifndef S2STransportation_h
 #define S2STransportation_h 1
 
-#include "G4Transportation.hh"
+#include <G4Transportation.hh>
 
 class S2STransportation : public G4Transportation
 {
 public:
-  S2STransportation() 
+  S2STransportation()
     :G4Transportation()
   {}
   ~S2STransportation()
   {}
-  
-  G4double AlongStepGetPhysicalInteractionLength( const G4Track &track,
-                                                  G4double previousStepSize, 
-                                                  G4double currentMinimumStep,
-                                                  G4double &currerntSafety, 
-                                                  G4GPILSelection *selection );
+
+  G4double AlongStepGetPhysicalInteractionLength(const G4Track &track,
+                                                 G4double previousStepSize,
+                                                 G4double currentMinimumStep,
+                                                 G4double &currerntSafety,
+                                                 G4GPILSelection *selection);
 };
 
 #endif
--- src/S2STransportation.cc
+++ src/S2STransportation.cc
@@ -1,34 +1,28 @@
-/*
-  "S2STransportation.cc"
-  
-  Modified by Toshi Gogami , 11Nov2014
-*/
+// -*- C++ -*-
 
 #include "S2STransportation.hh"
-#include "G4SystemOfUnits.hh"
 
+#include <G4SystemOfUnits.hh>
+
+namespace
+{
 const double MinimumStep = 5.0*mm;
+}
 
 G4double S2STransportation::
-AlongStepGetPhysicalInteractionLength( const G4Track &track,
-                                       G4double previousStepSize, 
-                                       G4double currentMinimumStep,
-                                       G4double &currentSafety, 
-                                       G4GPILSelection *selection ){
-  
-  // modified by tharada on 2022.06.17 to change from geant4.10.4.3 to geant4.10.6.2
-  /////////////////////////////////////////////////////////////////////////////
-  //  if( DoesGlobalFieldExist() &&currentMinimumStep>MinimumStep ){ //////////
-  if( DoesAnyFieldExist() &&currentMinimumStep>MinimumStep ){        //////////
-    ///////////////////////////////////////////////////////////////////////////
-    currentMinimumStep=MinimumStep;
+AlongStepGetPhysicalInteractionLength(const G4Track &track,
+                                      G4double previousStepSize,
+                                      G4double currentMinimumStep,
+                                      G4double &currentSafety,
+                                      G4GPILSelection *selection){
+  if(fAnyFieldExists && currentMinimumStep>MinimumStep){
+    currentMinimumStep = MinimumStep;
   }
-  
+
   return G4Transportation::
-    AlongStepGetPhysicalInteractionLength( track, 
-					   previousStepSize, 
-                                           currentMinimumStep,
-					   currentSafety, 
-                                           selection );
-  
+    AlongStepGetPhysicalInteractionLength(track,
+                                          previousStepSize,
+                                          currentMinimumStep,
+                                          currentSafety,
+                                          selection);
 }
