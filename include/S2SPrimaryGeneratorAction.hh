fc1bf7529681f4002306fb309cc21f06201c0bf8
Kohki-Amemiya <amemiya.kohki.q2@dc.tohoku.ac.jp>
2025-12-02 21:11:20 +0900

    clean up

--- include/S2SPrimaryGeneratorAction.hh
+++ include/S2SPrimaryGeneratorAction.hh
@@ -58,11 +58,11 @@ private:
   void GenerateRich7XiHSpectrum(G4Event* anEvent);
   void GenerateKH7XiHSpectrum(G4Event* anEvent);
   void GenerateE63_7LambdaLi(G4Event* anEvent, G4int MassNum);
+  void GenerateProton(G4Event* anEvent);
   void GenerateSigmaNCusp(G4Event* anEvent);
   void GenerateQFLambda(G4Event* anEvent);
   void GenerateQFSigmaZ(G4Event* anEvent);
   void GenerateQFSigmaP(G4Event* anEvent);
-  void GenerateDebugProton(G4Event* anEvent);
 };
 
 //_____________________________________________________________________________
